8ff730c52a077bcc5ad230c29ddce5a520ee12b9
unknown <yeasin18801@gmail.com>
2022-12-11 22:41:42 +0600

    web component service add extra code remove

--- resources/js/components/frontend/elements/Service.js
+++ resources/js/components/frontend/elements/Service.js
@@ -17,9 +17,6 @@ useEffect(()=>{
       <div className="container">
 
         <div className="section-title">
-          {/* <span>{ServiceData[0].service_header}</span>
-          <h2>{ServiceData[0].service_header}</h2>
-          <p>{ServiceData[0].service_title}</p> */}
           <span>{items && items[0].service_header}</span>
           <h2>{items && items[0].service_header}</h2>
           <p>{items && items[0].service_title}</p>
@@ -34,34 +31,7 @@ useEffect(()=>{
                 />)
 
             }
-
-           {/* <div className="row">
-            {
-                ServiceData.slice(1).map((Service,index) => <ServiceDetails key={index}
-                ServiceTitle={Service.service_title}
-                ServiceIcon={Service.service_icon}
-                ServiceDescription={Service.service_description}
-                />)
-
-            } */}
-
             {/* data render Another way  */}
-            {/* {
-
-                ServiceData.map((Service,index) => {
-                    return(
-                        <div className="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
-                            <div className="icon-box">
-                            <div className="icon"><i class={Service.service_icon}></i></div>
-                            <h4 className="title"><a href="">{Service.service_title}</a></h4>
-                            <p className="description">{Service.service_description}</p>
-                            </div>
-                        </div>
-                    )
-                })
-
-            } */}
-
         </div>
 
       </div>
