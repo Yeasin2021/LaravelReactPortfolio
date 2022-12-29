22056e7a525e840b1e75d9161ddfb57e2f15836f
unknown <yeasin18801@gmail.com>
2022-12-29 18:37:16 +0600

    message list show problem solve 1

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -39,7 +39,7 @@ class MessageController extends Controller
         Message::create([
             'name'          =>  $request->name,
             'email'         =>  $request->email,
-            'phone'         =>  $request->phone,
+            'subject'       =>  $request->subject,
             'message'       =>  $request->message
         ]);
         return response()->json(['status'=>200]);
