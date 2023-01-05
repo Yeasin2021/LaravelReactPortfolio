76b1debe1185ac3f620d20868389966d698c10b2
unknown <yeasin18801@gmail.com>
2023-01-05 16:17:07 +0600

    footer form component make 2

--- resources/js/components/back-end/admin/pages/footer/FooterForm.js
+++ resources/js/components/back-end/admin/pages/footer/FooterForm.js
@@ -3,7 +3,19 @@ import React from 'react'
 const FooterForm = () => {
   return (
     <div>
-
+        <div className="container">
+            <div className="row">
+                <div className="col-sm">
+                    One of three columns
+                </div>
+                <div className="col-sm">
+                    One of three columns
+                </div>
+                <div className="col-sm">
+                    One of three columns
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
