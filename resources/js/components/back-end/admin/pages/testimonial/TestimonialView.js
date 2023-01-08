1b123680761f5d3f2bda84e9cb228deb3a4dcf65
unknown <yeasin18801@gmail.com>
2023-01-08 15:13:12 +0600

    make TestimonialView component style

--- resources/js/components/back-end/admin/pages/testimonial/TestimonialView.js
+++ resources/js/components/back-end/admin/pages/testimonial/TestimonialView.js
@@ -7,12 +7,30 @@ const TestimonialView = () => {
         marginTop: '4%',
         }
   return (
-        <div class="container text-center">
-            <div class="row">
+        <div class="container">
+            <div class="row" style={ formStyleOne }>
                 <div class="col">
                     <div class="card">
                         <div class="card-body">
-                            This is some text within a card body.
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                    <th scope="col">SN</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Profession</th>
+                                    <th scope="col">Description</th>
+                                    <th scope="col">Image</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">1</th>
+                                        <td>Mark</td>
+                                        <td>Otto</td>
+                                        <td>@mdo</td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
