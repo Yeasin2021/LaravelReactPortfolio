e4a07d2f935bf9db75d50b98c5fe1c1d40e9027c
unknown <yeasin18801@gmail.com>
2023-01-08 15:03:43 +0600

    make TestimonialView component create

---
+++ resources/js/components/back-end/admin/pages/testimonial/TestimonialView.js
@@ -0,0 +1,24 @@
+
+import React from 'react'
+
+const TestimonialView = () => {
+    const formStyleOne = {
+        marginLeft: '300px',
+        marginTop: '4%',
+        }
+  return (
+        <div class="container text-center">
+            <div class="row">
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            This is some text within a card body.
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+  )
+}
+
+export default TestimonialView
