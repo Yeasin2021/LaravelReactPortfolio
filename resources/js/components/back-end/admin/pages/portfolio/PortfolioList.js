c001c6b03d31c7693aeedbc453a020aa83d3e9ca
unknown <yeasin18801@gmail.com>
2023-01-11 17:47:53 +0600

    Portfolio store method write 1

--- resources/js/components/back-end/admin/pages/portfolio/PortfolioList.js
+++ resources/js/components/back-end/admin/pages/portfolio/PortfolioList.js
@@ -83,7 +83,7 @@ const formStyleOne = {
                                                             <td>{item.title_two}</td>
                                                             <td>{item.link}</td>
                                                             {/* <td><p dangerouslySetInnerHTML={{ __html:item.description }}></p></td> */}
-                                                            <td><img src={`back-end/img/testimonial/${item.image}`} height="250px" width="250px"/></td>
+                                                            <td><img src={`back-end/img/portfolio/${item.image}`} height="250px" width="250px"/></td>
                                                             <td>
                                                                 <Link to={`/admin-testimonials/${item.id}`}><button className='btn btn-primary' onClick={editSound}><i className="bi bi-pencil"></i></button></Link>
                                                                 <button onClick={()=> deleteUser(item.id)}  className='btn btn-danger ms-3'><i className="bi bi-trash3"></i></button>
